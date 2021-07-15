507e99249af7480e773040dec01a9f3d8a56830c
wa5 <47422872+wa5@users.noreply.github.com>
2021-07-15 18:58:21 +0530

    Create app.js

---
+++ routings/advance routing/app.js
@@ -0,0 +1,33 @@
+var express=require('express');
+var app=express();
+
+//base route
+app.get("/",(req,res)=>{
+res.send("hello all")
+});
+
+//case 1 only string
+app.get("/about",(req,res)=>{
+    res.send("hello all im about")
+    });
+
+//case 2 only string
+app.get("/hom?e",(req,res)=>{//case 2 0 or more charecters of m home or hoe or hommmmme
+    res.send("hello all im case2")
+    });
+
+    //case 8 only params
+app.get("/cars/:carid",(req,res)=>{
+    res.send("hello all params example "+req.params.carid)
+    });
+
+
+
+
+
+
+
+
+app.listen(8001,()=>{
+    console.log("im working")
+})
