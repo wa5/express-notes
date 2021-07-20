39498b14ab1f7657d9e70925a424ce3d405dd3e4
wa5 <47422872+wa5@users.noreply.github.com>
2021-07-20 08:24:01 +0530

    Create app.js

---
+++ views-folder/ejs/injecting values in views/codes/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path=require("path")
+require('dotenv').config();
+app.set('port', process.env.PORT || 3000)
+
+const ejs=require("ejs");
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
+
+
+
+
+app
+    .route('/')
+    .get((req, res) => {
+        res.render("index")
+    });
+
+
+    app.get('/home',(req, res) => {
+        res.render("Home",{foo:'im data'})
+    });
+
+
+    app.get('/home/:data',(req, res) => {
+
+        res.render("Home",{
+            foo:'im data',
+            da:req.params.data} )
+    });
+
+
+
+app.get('*', (req, res) => {
+    var resObj = {
+        statusCode: 404,
+        statusMessage: "url not found"
+    }
+    res.send(resObj)
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'))
+})
